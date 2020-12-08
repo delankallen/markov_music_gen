b9a3533a7876087521f1e9d7bbf6ce88bb66c8ba
delankallen <delankallen@gmail.com>
2020-12-08 11:49:45 -0700

    starting UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function playMusic() {
-  
-}
+import {stopPlaying, genMusic} from './musicGen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button id='button' onClick={playMusic}>Click me!!!</button>
-      </header>
+      <header><h1>Markov Music Generation</h1></header>
+      <button id='button' onClick={genMusic}>Play Music</button>
+      <input type="button" value="Stop" onClick={stopPlaying}/>
     </div>
   );
 }
+
 
 export default App;
--- src/musicGen.ts
+++ src/musicGen.ts
@@ -1,4 +1,115 @@
 import Chain from 'markov-chains';
 import * as Tone from "tone";
-import bach from './bach_846.json';
+import {
+    parseMidi, Song
+} from "./parseMidi"
+import { NoteJSON } from '@tonejs/midi/dist/Note';
 
+const COMMA = ',';
+let toneContext = Tone.getContext();
+
+export const genMusic = async () => {
+    const song = await parseMidi("");
+    const parsedNotes = parseNotes(song)
+    playNotes(parsedNotes, song)
+}
+
+function parseNotes(song:Song) {
+    // const song = parseMidi(" ");
+    const notes: NoteJSON[] = song.tracks[0].notes;
+    if (song.tracks[1]) {
+        notes.push.apply(notes, song.tracks[1].notes);
+    }
+
+    const sixteenthNotes = [];
+
+    for (let time = 0; time <= song.length; time += song.noteInterval) {
+        const names = notes.filter(
+                note => time <= note.time && note.time < time + song.noteInterval
+            ).map(({
+                name
+            }) => name)
+            .sort();
+
+        sixteenthNotes.push(names.join(COMMA));
+    }
+
+    const phrases = [];
+
+    const enCopy = sixteenthNotes.slice(0);
+    while (enCopy.length > 0) {
+        phrases.push(enCopy.splice(0, song.phraseLength));
+    }
+
+    const phrasesIndexed = phrases.map(phrase =>
+        phrase.map((names, i) =>
+            names.length === 0 ? `${i}` : `${i}${COMMA}${names}`
+        )
+    );
+
+    return phrasesIndexed;
+}
+
+function playNotes(parsedNotes:string[][], song:Song) {
+    Tone.start();
+    toneContext.transport.PPQ = song.ppq;
+    let totalWaitTime = 0;
+    let tempWaitTime = 0;
+    const chain = new Chain(parsedNotes);
+    // const sampler = new Tone.Sampler({
+    //     urls: {
+    //         A1: "A1.mp3",
+    //         A2: "A2.mp3",
+    //     },
+    //     baseUrl: "https://tonejs.github.io/audio/casio/"
+    // }).toDestination();
+    const sampler = new Tone.Sampler({
+        urls: {
+            "C4": "C4.mp3",
+            "D#4": "Ds4.mp3",
+            "F#4": "Fs4.mp3",
+            "A4": "A4.mp3",
+        },
+        release: 1,
+        baseUrl: "https://tonejs.github.io/audio/salamander/",
+    }).toDestination();
+    sampler.volume.value = -10;
+    sampler.toDestination();
+    let schedulePhrase;
+
+    Tone.loaded().then(() => {
+        schedulePhrase = () => {
+            while (totalWaitTime <= song.length) {
+                var walker = chain.walk();
+                walker.forEach((noteName: String) => {
+                    const [t, ...names] = noteName.split(COMMA);
+                    const parsedT = Number.parseInt(t, 10);
+                    names.forEach(name => {
+                        const waitTime = parsedT * song.noteInterval + totalWaitTime;
+                        tempWaitTime = waitTime;
+                        sampler.triggerAttack(
+                            name,
+                            `+${waitTime+1+Math.random()*0.05-0.025}`
+                        )
+                    });
+                });
+
+                totalWaitTime = tempWaitTime + 1;
+            }
+        }
+        
+        toneContext.transport.scheduleRepeat(
+            schedulePhrase,
+            song.phraseLength * song.noteInterval
+        )
+
+        toneContext.transport.start();
+    })
+}
+
+export function stopPlaying() {
+    Tone.Transport.stop(0);
+    toneContext.transport.stop(0);
+}
+
+export default genMusic;
